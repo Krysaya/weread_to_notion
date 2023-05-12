ca5e3083983e6fd8f186f68f617572bc31580360
kris <hezhi66327@163.com>
2023-05-12 17:56:46 +0800

    Update weread.py

--- weread.py
+++ weread.py
@@ -226,12 +226,13 @@ def insert_to_notion(bookName, bookId, cover, sort, author,isbn,rating):
             "name": "读完" if markedStatus == 4 else "在读"}}
         properties["ReadingTime"] = {"rich_text": [
             {"type": "text", "text": {"content": format_time}}]}
+        if "continueBeginDate" in read_info:
+            properties["BeginDate"] = {"date": {"start": datetime.utcfromtimestamp(read_info.get(
+                "continueBeginDate")).strftime("%Y-%m-%d"), "time_zone": "Asia/Shanghai"}}
         if "finishedDate" in read_info:
             properties["Date"] = {"date": {"start": datetime.utcfromtimestamp(read_info.get(
                 "finishedDate")).strftime("%Y-%m-%d %H:%M:%S"), "time_zone": "Asia/Shanghai"}}
-         if "continueBeginDate" in read_info:
-            properties["BeginDate"] = {"date": {"start": datetime.utcfromtimestamp(read_info.get(
-                "continueBeginDate")).strftime("%Y-%m-%d"), "time_zone": "Asia/Shanghai"}}
+         
 
     icon = {
         "type": "external",
