b1c34c95ad8eaa9fe24092187122aa2bf86427cb
kris <hezhi66327@163.com>
2023-06-16 15:08:14 +0800

    Update weread.py
    2

--- weread.py
+++ weread.py
@@ -207,7 +207,7 @@ def insert_to_notion(bookName, bookId, cover, sort, author,isbn,rating,category)
         "ISBN": {"rich_text": [{"type": "text", "text": {"content": isbn}}]},
         "URL": {"url": f"https://weread.qq.com/web/reader/{calculate_book_str_id(bookId)}"},
         "Author": {"rich_text": [{"type": "text", "text": {"content": author}}]},
-        "category": {"select": {"name": category},
+        "category": {"select": {"name": category}},
         "Sort": {"number": sort},
         "Rating": {"number": rating},
         "Cover": {"files": [{"type": "external", "name": "Cover", "external": {"url": cover}}]},
