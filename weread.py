028cebdb5c3c4ce534ffb93747f99124a232211d
kris <hezhi66327@163.com>
2025-05-22 15:24:38 +0800

    Update weread.py

--- weread.py
+++ weread.py
@@ -13,12 +13,12 @@ from datetime import datetime
 import hashlib
 
 WEREAD_URL = "https://weread.qq.com/"
-WEREAD_NOTEBOOKS_URL = "https://i.weread.qq.com/user/notebooks"
-WEREAD_BOOKMARKLIST_URL = "https://i.weread.qq.com/book/bookmarklist"
-WEREAD_CHAPTER_INFO = "https://i.weread.qq.com/book/chapterInfos"
-WEREAD_READ_INFO_URL = "https://i.weread.qq.com/book/readinfo"
-WEREAD_REVIEW_LIST_URL = "https://i.weread.qq.com/review/list"
-WEREAD_BOOK_INFO = "https://i.weread.qq.com/book/info"
+WEREAD_NOTEBOOKS_URL = "https://weread.qq.com/user/notebooks"
+WEREAD_BOOKMARKLIST_URL = "https://weread.qq.com/book/bookmarklist"
+WEREAD_CHAPTER_INFO = "https://weread.qq.com/book/chapterInfos"
+WEREAD_READ_INFO_URL = "https://weread.qq.com/book/readinfo"
+WEREAD_REVIEW_LIST_URL = "https://weread.qq.com/review/list"
+WEREAD_BOOK_INFO = "https://weread.qq.com/book/info"
 
 
 def parse_cookie_string(cookie_string):
